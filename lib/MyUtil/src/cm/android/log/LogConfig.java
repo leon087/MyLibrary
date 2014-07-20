ed92798cdbeb86eed44bb845001519069fb6f9fa
leon087 <leo.zhuge@gmail.com>
2014-07-20 16:07:36 +0800

    新增日志功能xml配置信息模板

--- lib/MyUtil/src/cm/android/log/LogConfig.java
+++ lib/MyUtil/src/cm/android/log/LogConfig.java
@@ -2,15 +2,19 @@ package cm.android.log;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.android.BasicLogcatConfigurator;
 import ch.qos.logback.classic.android.LogcatAppender;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.rolling.RollingFileAppender;
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.InputStream;
 
 /**
  */
@@ -18,9 +22,12 @@ public class LogConfig {
 
     public static void test() {
         configLogback(Level.OFF, new File("/sdcard"));
+        //LogConfig.configLogbackByString(LogConfig.genXml(LogLevel.OFF,new File("/sdcard")));
         // BasicLogcatConfigurator.configureDefaultContext();
 
         Logger log = LoggerFactory.getLogger(LogConfig.class);
+        log.debug("gggg1,{},{}", "ggggg2", "ggggg3");
+        log.info("gggg1,{},{}", "ggggg2", "ggggg3");
         log.error("gggg1,{},{}", "ggggg2", "ggggg3");
     }
 
@@ -95,5 +102,86 @@ public class LogConfig {
         logcatAppender.start();
 
         root.addAppender(logcatAppender);
+    }
+
+    public static String genXml(LogLevel level, File logDir) {
+        String xmlConfig = "<configuration>" +
+                genFileString(logDir) +
+
+                "<appender name='LOGCAT' class='ch.qos.logback.classic.android.LogcatAppender'>" +
+                "<encoder>" +
+                "<pattern>[%thread:%logger:%method:%line] %msg%n</pattern>" +
+                "</encoder>" +
+                "</appender>" +
+
+                "<root level='" + level.getLevel() + "'>" +
+                genFileAppenderRef(logDir) +
+                "<appender-ref ref='LOGCAT' />" +
+                "</root>" +
+
+                "</configuration>";
+        return xmlConfig;
+    }
+
+    public static enum LogLevel {
+        ALL("ALL"), DEBUG("DEBUG"), INFO("INFO"), ERROR("ERROR"), OFF("OFF");
+
+        private String levelStr;
+
+        private LogLevel(String levelStr) {
+            this.levelStr = levelStr;
+        }
+
+        public String getLevel() {
+            return levelStr;
+        }
+    }
+
+    private static String genFileString(File logDir) {
+        if (logDir == null) {
+            return "";
+        }
+
+        String fileXmlConfig = "<property name='LOG_DIR' value='" + logDir.getAbsolutePath() + "'/>" +
+
+                "<appender name='FILE' class='ch.qos.logback.core.rolling.RollingFileAppender'>" +
+                "<file>${LOG_DIR}/logback.log</file>" +
+
+                "<rollingPolicy class='ch.qos.logback.core.rolling.TimeBasedRollingPolicy'>" +
+                "<fileNamePattern>${LOG_DIR}/logback.%d{yyyy-MM-dd}.log.gz</fileNamePattern>" +
+                "<maxHistory>7</maxHistory>" +
+                "</rollingPolicy>" +
+
+                "<encoder>" +
+                "<pattern>%date %-5level [%thread:%logger:%method:%line] - %msg%n</pattern>" +
+                "</encoder>" +
+
+                "</appender>";
+        return fileXmlConfig;
+    }
+
+    private static String genFileAppenderRef(File logDir) {
+        if (logDir == null) {
+            return "";
+        }
+        return "<appender-ref ref='FILE' />";
+    }
+
+    public static void configLogbackByString(String xmlString) {
+        // reset the default context (which may already have been initialized)
+        // since we want to reconfigure it
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        lc.reset();
+
+        JoranConfigurator config = new JoranConfigurator();
+        config.setContext(lc);
+
+        try {
+            InputStream stream = new ByteArrayInputStream(xmlString.getBytes());
+            config.doConfigure(stream);
+        } catch (Exception e) {
+            e.printStackTrace();
+            BasicLogcatConfigurator.configureDefaultContext();
+        }
     }
 }
