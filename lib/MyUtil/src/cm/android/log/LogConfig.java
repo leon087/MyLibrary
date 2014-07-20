bd66da987db7de174010c3d87970d696645cf178
leon087 <leo.zhuge@gmail.com>
2014-07-20 11:42:48 +0800

    更新logback机制：根据日期自动分割日志

--- lib/MyUtil/src/cm/android/log/LogConfig.java
+++ lib/MyUtil/src/cm/android/log/LogConfig.java
@@ -5,16 +5,19 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.android.LogcatAppender;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.FileAppender;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 /**
  */
 public class LogConfig {
 
     public static void test() {
-        configLogback(Level.OFF, true);
+        configLogback(Level.OFF, new File("/sdcard"));
         // BasicLogcatConfigurator.configureDefaultContext();
 
         Logger log = LoggerFactory.getLogger(LogConfig.class);
@@ -23,7 +26,7 @@ public class LogConfig {
 
     private static volatile boolean configFlag = false;
 
-    public static void configLogback(Level level, boolean isFileMode) {
+    public static void configLogback(Level level, File logDir) {
         if (configFlag) {
             return;
         }
@@ -39,38 +42,51 @@ public class LogConfig {
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
         lc.reset();
 
-        if (isFileMode) {
-            setFileMode(root, lc);
+        if (logDir != null) {
+            setFileMode(root, lc, logDir);
         }
 
         setLogcatMode(root, lc);
 
-        //Level:OFF
+        // Level:OFF
         root.setLevel(level);
     }
 
-    private static void setFileMode(ch.qos.logback.classic.Logger root, LoggerContext lc) {
+    private static void setFileMode(ch.qos.logback.classic.Logger root,
+                                    LoggerContext lc, File logDir) {
         // setup FileAppender
         PatternLayoutEncoder encoder1 = new PatternLayoutEncoder();
         encoder1.setContext(lc);
 
-        encoder1.setPattern("%date %level [%thread:%logger:%line] - %msg%n");
+        encoder1.setPattern("%date %-5level [%thread:%logger:%method:%line] - %msg%n");
         encoder1.start();
 
-        FileAppender<ILoggingEvent> fileAppender = new FileAppender<ILoggingEvent>();
+        RollingFileAppender<ILoggingEvent> fileAppender = new RollingFileAppender<ILoggingEvent>();
         fileAppender.setContext(lc);
-        fileAppender.setFile("/sdcard/logback.txt");
+        fileAppender.setFile(logDir.getAbsolutePath() + File.separator
+                + "logback.log");
         fileAppender.setEncoder(encoder1);
+
+        TimeBasedRollingPolicy<ILoggingEvent> policy = new TimeBasedRollingPolicy<ILoggingEvent>();
+        policy.setContext(lc);
+        policy.setMaxHistory(7);
+        policy.setFileNamePattern(logDir.getAbsolutePath() + File.separator
+                + "logback.%d{yyyy-MM-dd}.log.gz");
+        policy.setParent(fileAppender);
+        policy.start();
+
+        fileAppender.setRollingPolicy(policy);
         fileAppender.start();
 
         root.addAppender(fileAppender);
     }
 
-    private static void setLogcatMode(ch.qos.logback.classic.Logger root, LoggerContext lc) {
+    private static void setLogcatMode(ch.qos.logback.classic.Logger root,
+                                      LoggerContext lc) {
         // setup LogcatAppender
         PatternLayoutEncoder encoder2 = new PatternLayoutEncoder();
         encoder2.setContext(lc);
-        encoder2.setPattern("[%thread:%logger:%line] %msg%n");
+        encoder2.setPattern("[%thread:%logger:%method:%line] %msg%n");
         encoder2.start();
 
         LogcatAppender logcatAppender = new LogcatAppender();
