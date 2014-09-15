7bff1a5204928735e5084ab1bbc07b3f02e75ae4
leon087 <leo.zhuge@gmail.com>
2014-09-15 19:51:04 +0800

    更新salt

--- lib/MyUtil/src/main/java/cm/android/codec/SecureUtil.java
+++ lib/MyUtil/src/main/java/cm/android/codec/SecureUtil.java
@@ -1,14 +1,17 @@
 package cm.android.codec;
 
-import javax.crypto.spec.IvParameterSpec;
 import java.security.SecureRandom;
+
+import javax.crypto.spec.IvParameterSpec;
 
 public class SecureUtil {
     public static final String RANDOM_ALGORITHM = "SHA1PRNG";
 
     public static final byte[] IV_DEF = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
 
-    public static final byte[] SALT_DEF = {(byte) 0xA4, (byte) 0x0B, (byte) 0xC8, (byte) 0x34, (byte) 0xD6, (byte) 0x95, (byte) 0xF3, (byte) 0x13};
+    public static final byte[] SALT_DEF = {(byte) 0x53, (byte) 0x41,
+            (byte) 0x4C, (byte) 0x54, (byte) 0x5F, (byte) 0x44, (byte) 0x45,
+            (byte) 0x46};
 
     public static IvParameterSpec getIv(byte[] iv) {
         if (iv == null) {
