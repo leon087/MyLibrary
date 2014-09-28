b71345c7dc939520e3ec4b2a477657bda30b1032
leon087 <leo.zhuge@gmail.com>
2014-09-28 16:35:56 +0800

    新增Alarm工具类，修改intent

--- lib/MyFramework/src/main/java/cm/android/framework/core/DaemonManager.java
+++ lib/MyFramework/src/main/java/cm/android/framework/core/DaemonManager.java
@@ -1,13 +1,12 @@
 package cm.android.framework.core;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.SystemClock;
+
+import cm.android.sdk.AlarmUtil;
 
 public class DaemonManager {
-    public static final String ACTION_ALARM_WAKE_UP = "cm.android.framework.ACTION_ALARM_WAKE_UP";
+    public static final String ACTION_ALARM_WAKE_UP = "cm.android.framework.intent.action.ALARM_WAKE_UP";
 
     private DaemonManager() {
     }
@@ -20,33 +19,15 @@ public class DaemonManager {
         return Singleton.INSTANCE;
     }
 
-    private PendingIntent pendingIntent = null;
-
     public void startDaemon(Context context) {
-        PendingIntent sender = genPendingIntent(context);
-        long firstime = SystemClock.elapsedRealtime();
-        AlarmManager am = (AlarmManager) context
-                .getSystemService(Context.ALARM_SERVICE);
-
-        //循环发送广播
-        am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstime,
-                60 * 1000, sender);
+        Intent alarmIntent = new Intent(context, CoreReceiver.class);
+        alarmIntent.setAction(ACTION_ALARM_WAKE_UP);
+        AlarmUtil.start(context, alarmIntent, 0, 60 * 1000);
     }
 
     public void stopDaemon(Context context) {
-        PendingIntent sender = genPendingIntent(context);
-        AlarmManager am = (AlarmManager) context
-                .getSystemService(Context.ALARM_SERVICE);
-        am.cancel(sender);
-    }
-
-    private PendingIntent genPendingIntent(Context context) {
-        if (pendingIntent == null) {
-            Intent alarmIntent = new Intent(context, CoreReceiver.class);
-            alarmIntent.setAction(ACTION_ALARM_WAKE_UP);
-            pendingIntent = PendingIntent.getBroadcast(context, 0,
-                    alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        }
-        return pendingIntent;
+        Intent alarmIntent = new Intent(context, CoreReceiver.class);
+        alarmIntent.setAction(ACTION_ALARM_WAKE_UP);
+        AlarmUtil.stop(context, alarmIntent, 0);
     }
 }
