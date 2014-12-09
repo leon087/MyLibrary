6c51d6360923abc58dccfb55df56ae1945fe9095
leon087 <leo.zhuge@gmail.com>
2014-12-09 14:26:25 +0800

    gradle升级

--- app/MySample/src/main/java/cm/android/custom/MyServiceManager.java
+++ app/MySample/src/main/java/cm/android/custom/MyServiceManager.java
@@ -1,9 +1,6 @@
 package cm.android.custom;
 
 import android.content.Context;
-
-import com.android.server.am.ActivityManagerService;
-
 import cm.android.common.http.HttpLoader;
 import cm.android.common.http.MyHttp;
 import cm.android.framework.core.manager.CommonBaseManager;
@@ -45,8 +42,6 @@ public class MyServiceManager extends CommonBaseManager {
         HttpLoader.init(mContext);
 
         addService(new ExternalStorageReceiver(mContext, externalStorageListener));
-
-        ActivityManagerService ams = ActivityManagerService.self();
     }
 
     @Override
